3a2dab8a5046e09453fa65b2e963fef6fc1166d1
ElStabilini <el.stabilini@gmail.com>
2024-08-26 09:40:16 +0200

    changed fit script

--- fit.sh
+++ fit.sh
@@ -3,7 +3,7 @@
 # SBATCH directives (optional)
 #SBATCH --job-name=time_of_flight  # Job name
 #SBATCH --time=01:00:00            # Time limit
-#SBATCH --partition=iqm5q            # Partition name
+#SBATCH --partition=sim            # Partition name
 
 # Run the qq fit command
 qq fit /home/users/elisa.stabilini/protocolstest/output/res_spetroscopy
