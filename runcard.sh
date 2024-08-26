99f6bdf212f081b52daec4b9d675c7b56628beaa
ElStabilini <el.stabilini@gmail.com>
2024-08-26 07:23:06 +0200

    resonator spectroscopy test runcard

--- runcard.sh
+++ runcard.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # SBATCH directives (optional)
-#SBATCH --job-name=time_of_flight  # Job name
+#SBATCH --job-name=runcard_test0  # Job name
+#SBATCH --time=01:00:00            # Time limit
 #SBATCH --partition=iqm5q            # Partition name
 
 # Run the qq acquire command
-qq acquire /home/users/elisa.stabilini/protocolstest/res_spetroscopy.yml -o /home/users/elisa.stabilini/protocolstest/output/res_spetroscopy
+qq acquire /home/users/elisa.stabilini/protocolstest/res_spec_test.yml -o /home/users/elisa.stabilini/protocolstest/output/res_spec_test
